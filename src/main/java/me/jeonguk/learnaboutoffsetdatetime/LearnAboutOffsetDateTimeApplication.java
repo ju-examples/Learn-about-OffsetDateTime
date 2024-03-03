93461f611ccf3c4341d834d3c59e87b949a64745
JeongUk Park <dnr1105@gmail.com>
2024-03-03 23:52:34 +0900

    init:

---
+++ src/main/java/me/jeonguk/learnaboutoffsetdatetime/LearnAboutOffsetDateTimeApplication.java
@@ -0,0 +1,13 @@
+package me.jeonguk.learnaboutoffsetdatetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAboutOffsetDateTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnAboutOffsetDateTimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/jeonguk/learnaboutoffsetdatetime/LearnAboutOffsetDateTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jeonguk.learnaboutoffsetdatetime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnAboutOffsetDateTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
